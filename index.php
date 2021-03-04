75aab93ceca455fd30d8e071234f2f6d1c78f673
Erick Surya Dinata <ericksuryadinata@gmail.com>
2021-03-04 11:18:48 +0700

    add google site verification

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $G_C_SITE_KEY = getenv('G_C_SITE_KEY');
     <meta name="description" content="konsultasi gizi" />
     <meta name="author" content="konsultasigizi.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="i3jugcx0T15En1ICMZtDch-cLABX04M4wddsu61Cszc"/>
     <title>Konsultasi Gizi</title>
     <!-- Favicon -->
     <link rel="shortcut icon" href="assets/images/logo.png" />
